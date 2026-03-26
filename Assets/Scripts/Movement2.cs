2eb2b1583a203e699358f73a4cf5a24119742da1
GitUser1476 <ChristopherMoonie@gmail.com>
2026-03-25 17:27:17 -0700

    2 player, 2 cams, split screen, she's sick in the head bruv

---
+++ Assets/Scripts/Movement2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class Movement2 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 input;
+    public float speed = 4f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = input * speed;
+    }
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal2");
+        input.y = Input.GetAxisRaw("Vertical2");
+
+        input.Normalize();
+    }
+
+}
